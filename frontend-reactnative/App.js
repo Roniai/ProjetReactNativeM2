0cc6c9b7b09d886d5a3404ad01d469420b2c1acf
Fehizoroniaina Indrafo <fehizoroniainavalitera@gmail.com>
2023-04-29 11:57:04 +0300

    2.1 - ReactNative is working

--- frontend-reactnative/App.js
+++ frontend-reactnative/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{count}</Text>
+      <Button title="Increment" onPress={() => setCount(count + 1)}/>
       <StatusBar style="auto" />
     </View>
   );
